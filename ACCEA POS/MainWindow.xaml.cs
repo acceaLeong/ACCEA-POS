5360cf514795d6130c452be313377ad128d07c46
kl_sys001 <kl_sys001@KL-SYS001PC>
2024-04-22 14:51:14 +0800

    Add project files.

---
+++ ACCEA POS/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using ACCEA_POS.ViewModels;
+
+namespace ACCEA_POS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //DataContext = new MainVM();
+
+            InitializeComponent();
+
+            //cmbxLang.Items.Add("en");
+            //cmbxLang.Items.Add("ja");
+        }            
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    App.ChangeCulture("ja");
+        //}
+
+        private void cmbxLang_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //App.ChangeCulture(cmbxLang.SelectedValue as string);
+        }
+    }
+
+    //public partial class MainViewModel : ObservableObject
+    //{
+    //    //public MainViewModel()
+    //    //{
+
+    //    //}
+
+    //    //[ObservableProperty]
+    //    //string cmbxValue;
+
+    //    //[ICommand]
+    //    [RelayCommand]
+    //    void CmbxLang()
+    //    {
+    //        MessageBox.Show("abc");
+    //    }
+    //}
+}
