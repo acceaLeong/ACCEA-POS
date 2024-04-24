191ce08424fb87313034450d46874248785f905e
Marcus loo <Marcuslooek@gmail.com>
2024-04-24 14:33:46 +0800

    extract sales registration to own view added main menu navigation

--- ACCEA POS/Views/SalesRegistration.xaml.cs
+++ ACCEA POS/Views/SalesRegistration.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,23 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ACCEA_POS;
+namespace ACCEA_POS.Views;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+/// Interaction logic for SalesRegistration.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class SalesRegistration : Window
 {
-    public MainWindow()
+    public SalesRegistration()
     {
         InitializeComponent();
-    }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-
     }
 }
